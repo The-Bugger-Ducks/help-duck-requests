f26297d42e4b938157505830443cbc2b4903cbac
GabrielCamargoL <gabriel.cleite@outlook.com>
2022-03-28 22:02:25 -0300

    remove addlink method

--- src/main/java/com/helpduck/helpduckrequests/controller/RequestController.java
+++ src/main/java/com/helpduck/helpduckrequests/controller/RequestController.java
@@ -17,9 +17,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 
 import com.helpduck.helpduckrequests.entity.Request;
-import com.helpduck.helpduckrequests.model.RequestLinkAdder;
 import com.helpduck.helpduckrequests.model.RequestUpdater;
 import com.helpduck.helpduckrequests.repository.RequestRepository;
 
@@ -28,34 +28,31 @@ import com.helpduck.helpduckrequests.repository.RequestRepository;
 public class RequestController {
 	@Autowired
 	private RequestRepository repository;
-	@Autowired
-	private RequestLinkAdder linkAdder;
 
 	MongoTemplate mongoTemplate;
 
-	 @GetMapping("/")
-	 public ResponseEntity<List<Request>> getRequests() {
-	 	List<Request> requests = repository.findAll();
-	 	ResponseEntity<List<Request>> response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	@GetMapping("/")
+	public ResponseEntity<List<Request>> getRequests() {
+		List<Request> requests = repository.findAll();
+		ResponseEntity<List<Request>> response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
-	 	if (!requests.isEmpty()) {
-	 		response = new ResponseEntity<List<Request>>(requests, HttpStatus.FOUND);
-	 	}
-	 	return response;
-	 }
+		if (!requests.isEmpty()) {
+			response = new ResponseEntity<List<Request>>(requests, HttpStatus.FOUND);
+		}
+		return response;
+	}
 
-	 @GetMapping("/{id}")
-	 public ResponseEntity<Request> getRequest(@PathVariable String id) {
-	 	ResponseEntity<Request> response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		 
-	 	Optional<Request> requestOptional = repository.findById(id);
-	 	if (!requestOptional.isEmpty()) {
-	 		Request request = requestOptional.get();
-	 		linkAdder.addLink(request);
-	 		response = new ResponseEntity<Request>(request, HttpStatus.FOUND);
-	 	}
-	 	return response;
-	 }
+	@GetMapping("/{id}")
+	public ResponseEntity<Request> getRequest(@PathVariable String id) {
+		ResponseEntity<Request> response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+		Optional<Request> requestOptional = repository.findById(id);
+		if (!requestOptional.isEmpty()) {
+			Request request = requestOptional.get();
+			response = new ResponseEntity<Request>(request, HttpStatus.FOUND);
+		}
+		return response;
+	}
 
 	@PostMapping("/create")
 	public ResponseEntity<Request> createRequest(@RequestBody Request request) {
@@ -70,31 +67,31 @@ public class RequestController {
 		return new ResponseEntity<Request>(status);
 	}
 
-	 @PutMapping("/update")
-	 public ResponseEntity<HttpStatus> updateRequest(@RequestBody Request updatedRequest) {
-	 	Optional<Request> requestOptional = repository.findById(updatedRequest.getId());
-	 	HttpStatus status = HttpStatus.BAD_REQUEST;
+	@PutMapping("/update")
+	public ResponseEntity<HttpStatus> updateRequest(@RequestBody Request updatedRequest) {
+		Optional<Request> requestOptional = repository.findById(updatedRequest.getId());
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 
-	 	if (!requestOptional.isEmpty()) {
-	 		Request request = requestOptional.get();
-	 		RequestUpdater updater = new RequestUpdater();
-	 		updater.update(request, updatedRequest);
-	 		repository.save(request);
-	 		status = HttpStatus.OK;
-	 	}
-	 	return new ResponseEntity<HttpStatus>(status);
-	 }
+		if (!requestOptional.isEmpty()) {
+			Request request = requestOptional.get();
+			RequestUpdater updater = new RequestUpdater();
+			updater.update(request, updatedRequest);
+			repository.save(request);
+			status = HttpStatus.OK;
+		}
+		return new ResponseEntity<HttpStatus>(status);
+	}
 
-	 @DeleteMapping("/delete")
-	 public ResponseEntity<HttpStatus> deleteRequest(@RequestBody Request requestToDelete) {
-	 	// se só precisa do ID pra excluir, pq pedir um tipo "request"?
-	 	Optional<Request> requestOptional = repository.findById(requestToDelete.getId());
-	 	HttpStatus status = HttpStatus.BAD_REQUEST;
+	@DeleteMapping("/delete")
+	public ResponseEntity<HttpStatus> deleteRequest(@RequestBody Request requestToDelete) {
+		// se só precisa do ID pra excluir, pq pedir um tipo "request"?
+		Optional<Request> requestOptional = repository.findById(requestToDelete.getId());
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 
-	 	if (!requestOptional.isEmpty()) {
-	 		repository.deleteById(requestToDelete.getId());
-	 		status = HttpStatus.OK;
-	 	}
-	 	return new ResponseEntity<HttpStatus>(status);
-	 }
+		if (!requestOptional.isEmpty()) {
+			repository.deleteById(requestToDelete.getId());
+			status = HttpStatus.OK;
+		}
+		return new ResponseEntity<HttpStatus>(status);
+	}
 }
