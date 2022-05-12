99e87b6b683311565ddfc8f7088fe7424258206c
GabrielCamargoL <gabriel.cleite@outlook.com>
2022-05-11 23:17:32 -0300

    dynamic url solutions dev/prod enviroments

--- src/main/java/com/helpduck/helpducktickets/interfaces/SolutionRequests.java
+++ src/main/java/com/helpduck/helpducktickets/interfaces/SolutionRequests.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "solutionTicket", url = "http://localhost:8082/solutions")
+@FeignClient(name = "solutionTicket", url = "${url.solutions}")
 public interface SolutionRequests {
 
   @RequestMapping(method = RequestMethod.GET, value = "/ticket/{ticketId}")
