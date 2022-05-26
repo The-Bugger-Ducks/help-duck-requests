96b71638904a9f80d0db5e224093cbdc28ad8635
GabrielCamargoL <gabriel.cleite@outlook.com>
2022-05-26 20:44:14 -0300

    remove trys to get ordinal sort

--- src/main/java/com/helpduck/helpducktickets/entity/Ticket.java
+++ src/main/java/com/helpduck/helpducktickets/entity/Ticket.java
@@ -3,13 +3,9 @@ package com.helpduck.helpducktickets.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-
 import com.helpduck.helpducktickets.enums.PriorityLevelEnum;
 import com.helpduck.helpducktickets.enums.StatusEnum;
 
-import org.hibernate.annotations.Formula;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -26,11 +22,8 @@ public class Ticket {
 	private List<String> tags;
 	private Equipment equipment;
 
-	@Enumerated(EnumType.ORDINAL) // The default
 	private PriorityLevelEnum priorityLevel;
 
-	@Enumerated(EnumType.ORDINAL) // The default
-	@Formula("case status when 'awaiting' then 0 when 'underAnalysis' then 1 .when done then 2 end")
 	private StatusEnum status;
 
 	private User user;
