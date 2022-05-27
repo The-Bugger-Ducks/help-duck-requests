9b4019c3640c99e5e7518e6e45937cc333f26f13
Caio <caiovitordias1@gmail.com>
2022-05-27 12:42:18 -0300

    user entity fixed

--- src/main/java/com/helpduck/helpducktickets/entity/User.java
+++ src/main/java/com/helpduck/helpducktickets/entity/User.java
@@ -21,5 +21,5 @@ public class User {
   private String password;
 
   private RoleEnum role;
-  private RequestingDepartmentEnum requestingDepartmentEnum;
+  private RequestingDepartmentEnum requestingDepartment;
 }
